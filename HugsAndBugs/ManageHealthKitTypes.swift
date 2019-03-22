b4a67507212aea4685d1bc402eece6da0eb422af
Darin Williams <darinjswilliams@icloud.com>
2019-03-22 12:14:12 -0500

    initial commit

--- HugsAndBugs/ManageHealthKitTypes.swift
+++ HugsAndBugs/ManageHealthKitTypes.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import HealthKit
+
+public class ManageHealthKitTypes: NSObject{
+    
+    
+    public static let sharedInstance = ManageHealthKitTypes()
+    
+    
+    //MARK: Heath care store
+    public let healthStore = HKHealthStore()
+    
+    private let ageCharacteristicType = HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.dateOfBirth)
+    private let bmiQuantityType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass)
+    
+  
+    //MARK check to see if healthstore is available
+    public func isHealthKitAvailable() -> Bool{
+        guard let labResultType = HKObjectType.clinicalType(forIdentifier: .labResultRecord),
+           let  medicationsType = HKObjectType.clinicalType(forIdentifier: .medicationRecord) else {
+                fatalError("**ybabke ti create tge reqyested tyoes")
+        }
+
+        
+        return HKHealthStore.isHealthDataAvailable()
+    }
+    
+    public func requestHealthKitPermissionWithCompletion(completion: (Bool, NSError?) -> Void){
+        
+        
+        //mARK set up Sets
+        
+        
+    }
+}
